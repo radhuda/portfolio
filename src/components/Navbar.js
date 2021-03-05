0142c626584b15e08a04c59ccc903fc741541e8c
radhuda <rad.huda@gmail.com>
2021-03-05 18:53:11 -0500

    changed navbar design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
     
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."></img></a>
+        <a className="navbar-brand" href="#">Rad Huda</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
@@ -16,7 +16,16 @@ const Navbar = () => {
                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link" href="#">About Me</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Services</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Portfolio</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">contacts</a>
             </li>
             </ul>
         </div>
