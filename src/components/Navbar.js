a0fea9c19bf7f24ac88a9fc72d73e18ac814230d
radhuda <rad.huda@gmail.com>
2021-03-06 00:04:18 -0500

    nav add icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from "react"
 import logo from "../logo.png";
+//React FontAwesome Imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
@@ -7,7 +10,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#">Rad Huda</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <FontAwesomeIcon icon={faBars}/>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
