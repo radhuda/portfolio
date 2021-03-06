dd724c3e0b25e203976759bedbbb126ca85dde6c
root <root@radhuda.us-east1-b.c.vscode-engine.internal>
2021-03-06 22:21:53 +0000

    added header bg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#">Rad Huda</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <FontAwesomeIcon icon={faBars}/>
+            <FontAwesomeIcon icon={faBars} style/>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
