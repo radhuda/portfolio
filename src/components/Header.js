c30a26aba2a2147b80ca3fe0c5e1fa2aaedd69ba
root <rad.huda@gmail.com>
2021-03-06 18:06:20 +0000

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>Web Development and Website Promotion</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
