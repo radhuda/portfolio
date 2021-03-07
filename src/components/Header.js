f331ce7c323b50da99b71fb35de5ac9af3688e54
root <root@radhuda.us-east1-b.c.vscode-engine.internal>
2021-03-07 00:06:13 +0000

    added typed script package npm

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Typed from "react-typed"
 
 const Header = () => {
     return (
         <div className="header-wrapper">
             <div className="main-info">
-                <h1>Web Development and Website Promotion</h1>
+                <h1>Hello, I am Rad</h1>
+                <Typed
+                    className="typed-text"
+                    strings={["Cloud Engineer", "Data Engineer", "AWS Solution Architect", "Web Designer"]}
+                    typeSpeed = {40}
+                    backSpeed = {60}
+                    loop = {true}
+                />
             </div>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,8 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
     return (
     
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Rad Huda</a>
+        <nav className="navbar navbar-expand-lg navbar-light navbar-custom-bg">
+        {/* <a className="navbar-brand" href="#">Rad Huda</a> */}
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <FontAwesomeIcon icon={faBars} style/>
         </button>
